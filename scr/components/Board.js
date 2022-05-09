a95481e78da2d2c5644493089a7641c0c5ff8778
Daniel Brito <danielbrito@pq.uenf.br>
2022-05-08 22:22:05 -0300

    Applies Prettier

--- scr/components/Board.js
+++ scr/components/Board.js
@@ -1,33 +1,47 @@
-import { useState } from "react"
-import { View, StyleSheet, Button } from "react-native"
-import Square from "./Square"
+import { useState } from "react";
+import { View, StyleSheet, Button } from "react-native";
+import Square from "./Square";
 
+export default (props) => {
+  const [board, setBoard] = useState([
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+    NaN,
+  ]);
+  const [player, setPlayer] = useState(1);
+  function reset() {
+    setBoard([NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN]);
+  }
 
-export default props => {
-    const [board,setBoard] = useState([NaN,NaN,NaN,
-                                    NaN,NaN,NaN,
-                                    NaN,NaN,NaN])
-    const [player, setPlayer] = useState(1)
-    function reset(){
-        setBoard([NaN,NaN,NaN,
-                NaN,NaN,NaN,
-                NaN,NaN,NaN])
-    }
-    
-    return(
-        <View style={style.Flex}>
-            {board.map((_,i) => <Square index={i} key={i} board={board} 
-            setBoard={setBoard} player={player} setPlayer={setPlayer} squareStyle={style.Square}/>)}
-            <Button title="Reset" onPress={reset}/>
-        </View>
-    )
-}
-const style=StyleSheet.create({
-    Flex: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-        flexWrap: 'wrap',
-        width: '34%',
-    }
-})
+  return (
+    <View style={style.Flex}>
+      {board.map((_, i) => (
+        <Square
+          index={i}
+          key={i}
+          board={board}
+          setBoard={setBoard}
+          player={player}
+          setPlayer={setPlayer}
+          squareStyle={style.Square}
+        />
+      ))}
+      <Button title="Reset" onPress={reset} />
+    </View>
+  );
+};
+const style = StyleSheet.create({
+  Flex: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    width: "34%",
+  },
+});
--- scr/components/Square.js
+++ scr/components/Square.js
@@ -1,82 +1,71 @@
-import { useState } from "react"
-import { Pressable, Text, View } from "react-native"
-import { Button, StyleSheet, TouchableOpacity } from "react-native"
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+import { Button, StyleSheet, TouchableOpacity } from "react-native";
 
+export default (props) => {
+  const [squareState, setSquareState] = useState("_");
 
+  function checkWinner() {
+    let b = props.board;
 
-export default props => {
-  const [squareState, setSquareState] = useState("_") 
-
-  function checkWinner(){
-    let b = props.board
-
-    if(
-    (b[0] + b[1] + b[2])%3=== 0 || 
-    (b[3] + b[4] + b[5])%3=== 0 || 
-    (b[6] + b[7] + b[8])%3=== 0 || 
-    
-    (b[0] + b[3] + b[6])%3=== 0 || 
-    (b[1] + b[4] + b[7])%3=== 0 || 
-    (b[2] + b[5] + b[8])%3=== 0 || 
-
-    (b[1] + b[4] + b[8])%3=== 0 || 
-    (b[6] + b[4] + b[2])%3=== 0
-    ){
-      console.warn("Winner!")
-      return(-1) //TODO 1. stablish a logic to return the winner 2. break the logic into smaller steps
+    if (
+      (b[0] + b[1] + b[2]) % 3 === 0 ||
+      (b[3] + b[4] + b[5]) % 3 === 0 ||
+      (b[6] + b[7] + b[8]) % 3 === 0 ||
+      (b[0] + b[3] + b[6]) % 3 === 0 ||
+      (b[1] + b[4] + b[7]) % 3 === 0 ||
+      (b[2] + b[5] + b[8]) % 3 === 0 ||
+      (b[1] + b[4] + b[8]) % 3 === 0 ||
+      (b[6] + b[4] + b[2]) % 3 === 0
+    ) {
+      console.warn("Winner!");
+      return -1; //TODO 1. stablish a logic to return the winner 2. break the logic into smaller steps
     }
-    return(0)
-
+    return 0;
   }
 
-  function symbol(x){
-    if (isNaN(x)){return "_"}
-    return (x == 1? "X" : "O")
+  function symbol(x) {
+    if (isNaN(x)) {
+      return "_";
+    }
+    return x == 1 ? "X" : "O";
   }
 
-  function onPress(){
-    props.board[props.index] = props.player
-    props.setBoard(props.board)
-    props.setPlayer( ((props.player+1)%2) )
-    console.log(props.board)
-    checkWinner()
-    
+  function onPress() {
+    props.board[props.index] = props.player;
+    props.setBoard(props.board);
+    props.setPlayer((props.player + 1) % 2);
+    console.log(props.board);
+    checkWinner();
   }
 
-  return(   
-      <TouchableOpacity
-        style={style.button}
-        onPress={onPress}
-        index={props.index}>
-          <Text style={style.text}>
-            {symbol(props.board[props.index])}
-          </Text>
-        </TouchableOpacity>
+  return (
+    <TouchableOpacity
+      style={style.button}
+      onPress={onPress}
+      index={props.index}
+    >
+      <Text style={style.text}>{symbol(props.board[props.index])}</Text>
+    </TouchableOpacity>
+  );
+};
 
-
-  ) 
-  }
-
-  const style=StyleSheet.create({
+const style = StyleSheet.create({
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 20,
     paddingHorizontal: 60,
     borderRadius: 19,
-    elevation: 0,
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     height: 30,
     height: 30,
-  
-    
   },
   text: {
     fontSize: 20,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.01,
-    color: 'white',
+    color: "white",
   },
-
-  })
+});
